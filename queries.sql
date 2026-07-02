4ab493366124476f24c1ed50ba3c1e7624b9a806
Ahmed Saad <ahmed.saad.8326@gmail.com>
2026-07-02 12:42:57 +0300

    Add students querys and add and delete them from internships

--- queries.sql
+++ queries.sql
@@ -14,11 +14,27 @@ JOIN "company_supervisors" ON "company_supervisors"."id" = "internships"."compan
 JOIN "university_coordinators" ON "university_coordinators"."id" = "internships"."university_coordinator_id"
 ORDER BY "start_date" DESC;
 
--- Register a student in an internship
+-- Register student #1 in internship #1
+INSERT INTO "student_internships" ("student_id", "internship_id")
+VALUES (1, 1);
 
--- Withdraw a student from an internship
+-- Withdraw a student #1 from an internship #1
+DELETE FROM "student_internships" 
+WHERE "student_id" = 1 AND "internship_id" = 1;
 
--- View all internships for a student
+-- View all internships for a student #1
+SELECT "internships"."id", "internships"."title", "internships"."type", "internships"."location",
+       "companies"."title" AS "company", 
+       "company_supervisors"."first_name" || ' ' || "company_supervisors"."last_name" AS "company_supervisor",
+       "university_coordinators"."first_name" || ' ' || "university_coordinators"."last_name" AS "university_coordinator",
+       "internships"."start_date", "internships"."total_hours"
+FROM "internships"
+JOIN "student_internships" ON "student_internships"."internship_id" = "internships"."id"
+JOIN "companies" ON "companies"."id" = "internships"."company_id"
+JOIN "company_supervisors" ON "company_supervisors"."id" = "internships"."company_supervisor_id"
+JOIN "university_coordinators" ON "university_coordinators"."id" = "internships"."university_coordinator_id"
+WHERE "student_internships"."student_id" = 1
+ORDER BY "internships"."start_date" DESC;
 
 -- View all grades for a student
 
